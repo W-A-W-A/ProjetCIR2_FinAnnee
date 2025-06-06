37e1e85a974de9e631bcc2ab998b7d394642925d
AW <achille.wagner@isen-ouest.yncrea.fr>
2025-06-06 12:06:21 +0200

    feat(db): made connection testing more robust

--- back/db.php
+++ back/db.php
@@ -3,6 +3,7 @@
 $host = 'localhost'; // connection a la mariadb 
 $db   = 'solaire';
 $user = 'monuser';
+$altUser = 'admin'; // used during testing
 $pass = 'isen44';
 
 $options = [
@@ -13,7 +14,11 @@ $options = [
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass, $options);
 } catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(["error" => "Erreur de connexion : " . $e->getMessage()]);
-    exit;
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $altUser, $pass, $options);
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(["error" => "Erreur de connexion : " . $e->getMessage()]);
+        exit;
+    }
 }
